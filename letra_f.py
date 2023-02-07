bf73c7570e4905176b50e092a2e004e9b5462450
Alécio Santana <aleciosantana@gmail.com>
2023-02-07 20:41:40 -0300

    Removendo espaços em branco

--- letra_f.py
+++ letra_f.py
@@ -1,13 +1,13 @@
 def letra_f():
-  
+
   frase = input("Digite uma frase:")
-  
-  if "f" in frase: 
+
+  if "f" in frase:
     print("\nExite a letra F nessa frase\n")
     return
-    
+
   print("\nNão existe letra F nessa frase\n")
-    
+
   letra_f()
   return
 
