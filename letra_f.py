2acdfbec9ac2f926f2ccabdc1f52a0ae976b600f
Flávio Júnior <flaviojunior9400@gmail.com>
2023-02-07 19:57:01 -0300

    Primeiro commite

---
+++ letra_f.py
@@ -0,0 +1,13 @@
+def letra_f():
+  frase = input("Digite uma frase:")
+  
+  if "f" in frase: 
+    print("\nexite a letra F nessa frase\n")
+    return
+    
+  print("\nNao existe letra F nessa frase\n")
+    
+  letra_f()
+  return
+
+letra_f()
