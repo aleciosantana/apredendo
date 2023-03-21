6487f44b7f413c09fbe16e6fe48ea4b108f0a82e
Flávio Júnior <flaviojunior9400@gmail.com>
2023-03-21 19:52:39 -0300

    operadores_++_--

---
+++ operadores_++_--.py
@@ -0,0 +1,10 @@
+# 27-operadores ++ e -- incremento e decremendo
+x = int(input("\nDigite numero para incremento: "))
+y = int(input("\nDigite numero para decremento: "))
+
+print("\n X = ",x)
+x += 1
+print("\nY = ",y)
+y -= 1
+print("\nO valor de X = ",x)
+print("\nO valor de X = ",y)
