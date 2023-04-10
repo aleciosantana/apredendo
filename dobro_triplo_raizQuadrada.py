ffd04ca7c43ed2a6fa916154160b3312e6ad4e74
Flávio Júnior <flaviojunior9400@gmail.com>
2023-04-10 19:27:53 -0300

    Achando o dobro o triplo e a Raiz quadrada

---
+++ dobro_triplo_raizQuadrada.py
@@ -0,0 +1,11 @@
+
+"""criando um algoritimo que leia o numero e imprima o 
+seu dobro, o triplo e a raiz quadrada"""
+
+n1 = int(input("\nDigite um numero: "))
+dobro = n1 * 2
+triplo = n1 * 3
+raiz_quadrada = n1 ** (1/2)
+print ("O dobro é: {}".format(dobro))
+print ("O triplo é: {}".format(triplo))
+print ("A raiz Quadrada é: {:.2f}".format(raiz_quadrada))
