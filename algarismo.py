02a7bb80e5d3e24360b052bd543805aa452c015c
Flávio Júnior <flaviojunior9400@gmail.com>
2023-04-25 21:21:02 -0300

    imprimir algarismo indicado na casa das dezenas

---
+++ algarismo.py
@@ -0,0 +1,5 @@
+# 23- entre com um numero de tres casas e imprima o algarismo da casa da dezena.
+a = input("\nDigite numero de três casas: ")
+b = a[-2]
+print ("\nAlgarismo da casa das dezenas: ", b)
+
