0b7c2b8e94dbc170a7b2f284a2617f96e37f8d05
Flávio Júnior <flaviojunior9400@gmail.com>
2023-05-18 18:54:13 -0300

    mostras o primeiro e o ultimo item do nome

---
+++ mostra_primeiro_e_ultimo_jtem_do_nome.py
@@ -0,0 +1,4 @@
+nome = str(input("Digite o nome completo da Pessoa:")).strip().split()
+print ("Òla!!!")
+print ("Seu primeiro neme é: {}".format(nome[0]))
+print ("Seu ultimo nome é: {}.".format(nome[len(nome)-1]))
